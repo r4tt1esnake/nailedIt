2ab25c95c86928a5f4a03efc8f3a38683c0ec0d2
r4tt1esnake <r4tt1esnake@users.noreply.github.com>
2018-01-05 20:42:09 -0800

    I DONE IT
    HOLY

--- nailedIt.cpp
+++ nailedIt.cpp
@@ -3,13 +3,22 @@
 #include <vector>
 
 std::vector<int> getPieces();
-std::vector<int> findPossibleBoardLengths(std::vector<int> pieceList);
-int findFenceLength(std::vector<int> pieceList, int boardLength);
-std::vector<int> quickSortVector(std::vector<int> unsortedVector);
+std::vector<int> getUniqueFenceLengthCount(std::vector<int> pieceList);
+int findViableHeights(std::vector<int> uniqueFenceLengthCount);
+int findMaxFenceLength(std::vector<int> uniqueFenceLengthCount);
 
 int main() {
 
 	std::vector<int> pieceList = getPieces();
+	std::vector<int> uniqueList = getUniqueFenceLengthCount(pieceList);
+
+	for(int i = 0; i < uniqueList.size(); i++) {
+		std::cout << uniqueList[i] << "\n";
+	}
+
+	std::cout << findMaxFenceLength(uniqueList);
+	std::cout << " ";
+	std::cout << findViableHeights(uniqueList) << "\n";
 
 }
 
@@ -20,7 +29,7 @@ std::vector<int> getPieces() {
 	*/
 
 	std::ifstream file;
-	file.open("c.in");
+	file.open("s3.05.in");
 
 	int pieceCount;
 	file >> pieceCount;
@@ -37,14 +46,48 @@ std::vector<int> getPieces() {
 
 }
 
-std::vector<int> quickSortVector(std::vector<int> unsortedVector) {
+std::vector<int> getUniqueFenceLengthCount(std::vector<int> pieceList) {
 
-	std::vector<int> sortedVector;
+	std::vector<int> elementCount;
+	elementCount.resize(4000);
 
-	/*
-		Implement quicksort magickery.
-	*/
+	for(int i = 0; i < pieceList.size(); i++) {
+		for(int j = i + 1; j < pieceList.size(); j++) {
+			elementCount[pieceList[i] + pieceList[j]]++;
+		}
+	}
 
-	return sortedVector;
+	return elementCount;
 
 }
+
+int findViableHeights(std::vector<int> uniqueFenceLengthCount) {
+
+	int count = 0;
+
+	for(int i = 0; i < uniqueFenceLengthCount.size(); i++) {
+		if(uniqueFenceLengthCount[i] != 0) {
+			count++;
+		}
+	}
+
+	return count;
+
+}
+
+int findMaxFenceLength(std::vector<int> uniqueFenceLengthCount) {
+
+	int temp;
+
+	for(int i = 0; i < uniqueFenceLengthCount.size(); i++) {
+		if(uniqueFenceLengthCount[i] > uniqueFenceLengthCount[i + 1]) {
+			temp = uniqueFenceLengthCount[i];
+			uniqueFenceLengthCount[i] = uniqueFenceLengthCount[i + 1];
+			uniqueFenceLengthCount[i + 1] = temp;
+		}
+	}
+
+	return uniqueFenceLengthCount[uniqueFenceLengthCount.size() - 1];
+
+}
+
